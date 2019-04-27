0c1b1426a0fea7e5ac2774cb609d5df8c24421b9
divyani <divyani.pamulapati@gmail.com>
2019-04-27 21:24:16 +0200

    Update App.java

--- src/main/java/calc/App.java
+++ src/main/java/calc/App.java
@@ -3,16 +3,31 @@ package calc;
 public class App {
 	public App(){
 		}
-	public int add(int a, int b) {
-		
-		return -1;
+	public int add(int a, int b){
+
+		return a+b;
 	}
-	
-		public int sub(int a, int b) {
-			return -100;
+	public int sub(int a, int b) {
+		return a-b;
+	}
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	public int div(int a, int b) {
+		if(b==0) {
+			return 0;
 		}
-			
+		else {
+			return a/b;
 		}
-	
-	
+		}
+	public Object fact(int a) {
+		// TODO Auto-generated method stub
+		return a*(a-1);
+	}
+	public int remaind(int a, int b) {
+		return a%b;
+	}
+}
+
 	
