f8ed4c18d73dd108f8a1c70a08d5545e7af64f1b
divyani <divyani.pamulapati@gmail.com>
2019-04-27 04:21:09 +0200

    Update App.java

--- src/main/java/calc/App.java
+++ src/main/java/calc/App.java
@@ -6,40 +6,18 @@ public class App {
 	}
 
 	public int add(int a, int b) {
-		  return a+b;
+		  return 0;
 	}
 
 	public int sub(int a, int b) {
-		return a - b;
+		return 0;
 	}
 
 	public int mul(int a, int b) {
-		return a * b;
+		return 0;
 	}
 
-	public int div(int a, int b) {
-		if (b == 0) {
-			return 0;
-		} else {
-			return a / b;
-		}
-	}
-
-	public Object Square(int a) {
-		return a * a;
-	}
-
-	public Object fact(int a) {
-		return a * (a - 1);
-	}
-
-	public int sqrt(int a) {
-		int b = (int) Math.sqrt(a);
-		System.out.println("sqrt is" + b);
-		return (int) Math.sqrt(a);
-	}
-
-	public int remaind(int a, int b) {
-		return a%b;
-	}
+	
 }
+
+	
