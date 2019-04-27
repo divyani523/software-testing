5a2cd9662ddf341a2950f467bb8ec72ea3f27862
divyani <divyani.pamulapati@gmail.com>
2019-04-27 04:53:32 +0200

    Update App.java

--- src/main/java/calc/App.java
+++ src/main/java/calc/App.java
@@ -1,15 +1,26 @@
 package calc;
 
 public class App {
+	public App(){
+		}
+	public int add(int a, int b){
 
-	public App() {
-	}
-
-	public int add(int a, int b) {
-		  return 0;
+		return 0;
 	}
 
 	public int sub(int a, int b) {
-		return 0;
+		return a-b;
+	}
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	public int div(int a, int b) {
+		if(b==0) {
+			return 0;
+		}
+		else {
+			return a/b;
+		}
 	}
 }
+	
