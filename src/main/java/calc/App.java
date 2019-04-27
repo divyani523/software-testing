94a25c76a93c18c32210f0d8353a6348f905003b
divyani <divyani.pamulapati@gmail.com>
2019-04-27 17:42:10 +0200

    Update App.java
    pushing code so that test fails and build sucess

--- src/main/java/calc/App.java
+++ src/main/java/calc/App.java
@@ -3,31 +3,16 @@ package calc;
 public class App {
 	public App(){
 		}
-	public int add(int a, int b){
-
+	public int add(int a, int b) {
+		
 		return -1;
 	}
-	public int sub(int a, int b) {
-		return -100;
-	}
-	public int mul(int a, int b) {
-		return -200;
-	}
-	/*public int div(int a, int b) {
-		if(b==0) {
-			return 0;
+	
+		public int sub(int a, int b) {
+			return -100;
 		}
-		else {
-			return a/b;
+			
 		}
-		}
-	public Object fact(int a) {
-		// TODO Auto-generated method stub
-		return a*(a-1);
-	}
-	public int remaind(int a, int b) {
-		return a%b;*/
-
-}
-
+	
+	
 	
