1a7d9643946341b216c978012670c03942f6fc37
divyani <divyani.pamulapati@gmail.com>
2019-04-27 07:05:38 +0200

    Update App.java
    extended java project with new code

--- src/main/java/calc/App.java
+++ src/main/java/calc/App.java
@@ -7,21 +7,27 @@ public class App {
 
 		return a+b;
 	}
-public int sub(int a, int b) {
-	return a-b;
-}
-public int mul(int a, int b) {
-	return a*b;
-}
-public int div(int a, int b) {
-	if(b==0) {
-		return 0;
+	public int sub(int a, int b) {
+		return a-b;
 	}
-	else {
-		return a/b;
-		
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	public int div(int a, int b) {
+		if(b==0) {
+			return 0;
+		}
+		else {
+			return a/b;
+		}
+		}
+	public Object fact(int a) {
+		// TODO Auto-generated method stub
+		return a*(a-1);
+	}
+	public int remaind(int a, int b) {
+		return a%b;
 	}
 }
 
-}
-
+	
