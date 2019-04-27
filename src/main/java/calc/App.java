de502472fe7ad24c284024fa38df96871fdaabc6
divyani <divyani.pamulapati@gmail.com>
2019-04-27 04:35:30 +0200

    Update App.java

--- src/main/java/calc/App.java
+++ src/main/java/calc/App.java
@@ -6,23 +6,10 @@ public class App {
 	}
 
 	public int add(int a, int b) {
-		  return 0;
+		  return a+b;
 	}
 
 	public int sub(int a, int b) {
-		return 0;
+		return a - b;
 	}
-
-	public int mul(int a, int b) {
-		return 0;
-	}
-
-	public int div(int a, int b) {
-		if (b == 0) {
-			return 1;
-		} else {
-			return 0;
-		}
-	
-}
 }
