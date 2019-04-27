89a2ca12d29da4b3d76a7fa585186b458b16d202
divyani <divyani.pamulapati@gmail.com>
2019-04-27 17:35:19 +0200

    Update App.java

--- src/main/java/calc/App.java
+++ src/main/java/calc/App.java
@@ -8,10 +8,26 @@ public class App {
 		return -1;
 	}
 	public int sub(int a, int b) {
-		return -1;
+		return -100;
 	}
 	public int mul(int a, int b) {
-		return -1;
-		
+		return -200;
 	}
+	/*public int div(int a, int b) {
+		if(b==0) {
+			return 0;
+		}
+		else {
+			return a/b;
+		}
+		}
+	public Object fact(int a) {
+		// TODO Auto-generated method stub
+		return a*(a-1);
 	}
+	public int remaind(int a, int b) {
+		return a%b;*/
+
+}
+
+	
